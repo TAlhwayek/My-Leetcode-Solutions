ae0eafdd20a946e5481a5180346dd7a178d79cbe
TAlhwayek <60246873+TAlhwayek@users.noreply.github.com>
2023-10-21 14:54:44 -0700

    Problem is complete
    Includes all necessary comments

---
+++ validAnagram/main.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Tony Alhwayek on 10/21/2023.
+//
+
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+// Runtime:
+// Sorting = O(k *logk) where k = length of the string
+// Comparing = O(k)
+// Total = O(k * logk)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // Sort both strings
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        // Compare them
+        return s == t;
+    }
+};
